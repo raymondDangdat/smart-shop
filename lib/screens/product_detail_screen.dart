92db3f47101cb8ba7588f97781d763afb749fab1
Dangdat <raymonddangdat@gmail.com>
2021-02-12 04:15:13 +0100

    Implemented named routing

---
+++ lib/screens/product_detail_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:shop/screens/product_overview.dart';
+class ProductDetailScreen extends StatelessWidget {
+  // final String title;
+  // final double price;
+  // ProductDetailScreen(this.title, this.price);
+  static const routeName = '/product-details';
+  @override
+  Widget build(BuildContext context) {
+    final productId = ModalRoute.of(context).settings.arguments as String;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Smart Shop"),
+        centerTitle: true,
+      ),
+    );
+  }
+}
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,26 +1,36 @@
 import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
   final String id;
   final String title;
   final String imageUrl;
+  final String price;
 
-  ProductItem(this.id, this.title, this.imageUrl);
+  ProductItem(this.id, this.title, this.imageUrl, this.price);
 
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(10.0),
       child: GridTile(
-          child: Image.network(
+          child: InkWell(
+            onTap: (){
+              Navigator.pushNamed(context, ProductDetailScreen.routeName, arguments: id);
+            },
+            child: Image.network(
         imageUrl,
         fit: BoxFit.cover,
       ),
+          ),
       footer: GridTileBar(
         leading: IconButton(icon: Icon(Icons.favorite), onPressed: (){}, color: Theme.of(context).accentColor,),
         trailing: IconButton(icon: Icon(Icons.shopping_cart), onPressed: (){}, color: Theme.of(context).accentColor,),
         backgroundColor: Colors.black87,
-        title: Text(title, textAlign: TextAlign.center,),),),
+        title: Text('₦ $price', textAlign: TextAlign.center, style: TextStyle(fontSize: 10.0, fontWeight: FontWeight.bold),),),
+      header: GridTileBar(
+    title: Text(title, textAlign: TextAlign.center,),),
+    )
     );
   }
 }
