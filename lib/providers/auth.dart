4818995425b97523677a0736988874e42ef031a0
Dangdat <raymonddangdat@gmail.com>
2021-02-16 07:21:17 +0100

    Implemented login  with REST Auth API

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -8,9 +8,8 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signUp(String email, String password) async {
-    const url =
-        "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCfCfthSxAKcXiioCP0VyIpUvALvFDbNJY";
+  Future<void> _authenticate(String email, String password, String urlSegment) async{
+    final url = "https://identitytoolkit.googleapis.com/v1/$urlSegment?key=AIzaSyCfCfthSxAKcXiioCP0VyIpUvALvFDbNJY";
     final response = await http.post(url,
         body: json.encode({
           'email': email,
@@ -18,5 +17,13 @@ class Auth with ChangeNotifier {
           'returnSecureToken': true,
         }));
     print(json.decode(response.body));
+  }
+
+  Future<void> signUp(String email, String password) async {
+    return _authenticate(email, password, 'accounts:signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'accounts:signInWithPassword');
   }
 }
