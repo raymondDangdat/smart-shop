8eb71307733d27203ba18b73131e6b1d09cb8c63
Dangdat <raymonddangdat@gmail.com>
2021-02-16 08:15:29 +0100

    Handled authentication errors

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -2,14 +2,33 @@ import 'dart:convert';
 
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
+import '../models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> _authenticate(String email, String password, String urlSegment) async{
-    final url = "https://identitytoolkit.googleapis.com/v1/$urlSegment?key=AIzaSyCfCfthSxAKcXiioCP0VyIpUvALvFDbNJY";
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        "https://identitytoolkit.googleapis.com/v1/$urlSegment?key=AIzaSyCfCfthSxAKcXiioCP0VyIpUvALvFDbNJY";
+
+    try {
+      final response = await http.post(url,
+          body: json.encode({
+            'email': email,
+            'password': password,
+            'returnSecureToken': true,
+          }));
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpExceptions(responseData['error']['message']);
+      }
+    } catch (error) {
+      throw error;
+    }
+
     final response = await http.post(url,
         body: json.encode({
           'email': email,
